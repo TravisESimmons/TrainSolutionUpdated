8f176f4264cfdf424fc90253ba66fad649d984e7
Travis Simmons <113370264+TravisESimmons@users.noreply.github.com>
2022-12-06 23:40:33 -0700

    Incomplete, final commit.

--- TrainWatchLibrary/BLL/RollingStockServices.cs
+++ TrainWatchLibrary/BLL/RollingStockServices.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TrainWatchSystem.DAL;
 using TrainWatchSystem.Entities;
 
@@ -32,6 +33,13 @@ namespace TrainWatchSystem.BLL
 
         }
 
+        public List<RailCarType> RailCarTypeList()
+
+        {
+           return _context.RailCarTypes.OrderBy(x => x.Name).ToList();
+           
+
+        }
 
         public List <RollingStock> GetByPartial(string SearchArg, int pageNumber,
                                                         int pageSize,
@@ -40,26 +48,192 @@ namespace TrainWatchSystem.BLL
             IEnumerable<RollingStock> info = _context.RollingStocks
                 .Where(x => x.ReportingMark.Contains(SearchArg))
                 .OrderBy(x => x.ReportingMark);
-               
-       
 
             totalCount = info.Count();
-            //  Determine the number of rows to skip
-            //  THis skipped count reflects the rows of the previous pages
-            //  Remember the pagenumber is a natural number (1,2,3,....)
-            //  This needs to be treated as an index (natural number -1)  Zero base
-            //  The number of rows to skip is index * pagesize
+        
             int skipRows = (pageNumber - 1) * pageSize;
-            //  Return only the required number of rows.
-            //  This will be done using filters belonging to LINQ
-            //  Use the filter .Skip(n) to skip over n rows from the beginning of a collection
-            //  Use the filter .Take(n) to take the next n rows from a collection
+
             return info.Skip(skipRows).Take(pageSize).ToList();
 
-            //  This is the return statement that would be used IF no paging is being implemented
+          
         }
-       
-        
+
+        public List<RollingStock> GetByRailCarTypeID(int searcharg, int pageNumber,
+                                                       int pageSize,
+                                                       out int totalCount)
+        {
+            IEnumerable<RollingStock> info = _context.RollingStocks
+                .Where(x => x.RailCarTypeID == searcharg)
+                .OrderBy(x => x.ReportingMark);
+
+            totalCount = info.Count();
+
+            int skipRows = (pageNumber - 1) * pageSize;
+
+            return info.Skip(skipRows).Take(pageSize).ToList();
+
+
+        }
+
+        public RollingStock GetByID(string searcharg)
+        {
+            RollingStock info = _context.RollingStocks
+                                .Where(x => x.ReportingMark.Equals(searcharg))
+                                .FirstOrDefault();
+            return info;
+        }
+
+        public string RollingStock_AddStock(RollingStock item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Rolling Stock data is missing");
+            }
+            //this is an optional sample of validation of incoming data
+            RollingStock exists = _context.RollingStocks
+                            .Where(x => x.ReportingMark.Equals(item.ReportingMark) &&
+                                        x.Owner.Equals(item.Owner) &&
+                                        x.Capacity.Equals(item.Capacity) &&
+                                        x.InService == item.InService)
+                            .FirstOrDefault();
+            if (exists != null)
+            {
+                throw new Exception($"{item.ReportingMark}");
+            }
+
+            //stage the data in local memory to be submitted to the database for 
+            //  storage
+            // a) what DbSet  
+            // b) the action
+            // c) indicate the data involved.
+
+            //IMPORTANT: the data is in LOCAL memory; it has NOT!!!!!!! yet been sent
+            //          to the database.
+            //          THEREFORE: at this time, there is NO!!!!! primary key value (except
+            //              for the system default (numerics -> 0)
+            _context.RollingStocks.Add(item);
+
+            // commit the LOCAL data to the database
+
+            //IF there are validation annotations on your Entity
+            //  they will be executed during the SaveChanges
+            _context.SaveChanges();
+
+            //AFTER the commit, your pkey value will NOW be available to you
+            return item.ReportingMark;
+        }
+
+        //update
+        //update can also have design considerations for validation
+        //update may request that you check the record of interest is still
+        //  on the database
+
+        public string RollingStock_UpdateStock(RollingStock item)
+        {
+            //for an update, you MUST have the pkey value on your instance
+            //if not, it will not work.
+
+            // ***** WARNING ****** 
+            // can cause PROBLEMS when being used with EntityEntry<t> processing
+
+            //this technique returns an instance (object)
+            //Product exists = _context.Products
+            //                    .Where(x => x.ProductID == item.ProductID)
+            //                    .FirstOrDefault();
+            //if (exists == null)
+            //{
+            //    throw new Exception($"{item.ProductName} with a size of {item.QuantityPerUnit}" +
+            //    $" from the selected supplier is not on file");
+            //}
+
+            // ****** BETTER ************
+            // this does NOT actually return an instance and thus has no
+            //   CONFLICT with using EntityEntry<T>
+
+            //this technique does the search BUT returns only a boolean of success
+            bool exists = _context.RollingStocks.Any(x => x.ReportingMark == item.ReportingMark);
+            //if(!_context.Products.Any(x => x.ProductID == item.ProductID))
+            if (!exists)
+            {
+                throw new Exception($"");
+            }
+
+            //stage the update
+            EntityEntry<RollingStock> updating = _context.Entry(item);
+            //flag the entity to be modified
+            updating.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+
+            //during the commit, SaveChanges() returns the number of rows affected
+            _context.SaveChanges();
+            return item.ReportingMark;
+        }
+
+        public string RollingStock_DeleteStock(RollingStock item)
+        {
+            //for an delete, you MUST have the pkey value on your instance
+            //if not, it probably will not work as expected.
+
+            //this technique returns an instance (object)
+            //Product exists = _context.Products
+            //                    .Where(x => x.ProductID == item.ProductID)
+            //                    .FirstOrDefault();
+
+            //this technique does the search BUT returns only a boolean of success
+            bool exists = _context.RollingStocks.Any(x => x.ReportingMark == item.ReportingMark);
+
+            //DEPENDING on which technique you use, your error test will be different
+            //one:   if (exists == null) {....}
+            if (!exists)
+            {
+                throw new Exception($"");
+            }
+
+            //Removing a record from your database maybe a
+            // a) phyiscal act
+            //   OR
+            // b) logical act
+
+            // a) if you wish the record to be "phyiscally remove from the databae
+            //    you will use the staging of .Deleted
+            //    if the record being removed from the database is a "parent" record
+            //      (referenced in a foreign key), the delete WILL FAIL in a relational
+            //      database IF there are existing "children" or the record
+            //    sql "parent records cannot be deleted if children exist"
+            //    the decission to automatically remove "children" is a system design decission 
+
+            //stage the phyiscal delete
+            //EntityEntry<Product> deleting = _context.Entry(item);
+            //flag the entity to be deleted
+            //.State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+
+            // b) Locial delete
+            //      this is where you do not or cannot phyiscally remove a record from
+            //      the database.
+            //      This decission is based on existing best practice business rules OR
+            //          set by government regulations
+            //      this type of delete is done so the "flagged" data is NOT used in
+            //          daily processing
+            //
+            //   this type of delete will actually be an update the attribute (property) 
+            //      of the record.
+            //   Look for attributes such as Active, Discontinued, a special date ReleaseDate
+
+            // Product is a logical delete (Discontinued = true;)
+
+            //stage the logical delete
+            item.InService = true;
+            EntityEntry<RollingStock> updating = _context.Entry(item);
+            //flag the entity to be deleted
+            updating.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+
+
+            //during the commit, SaveChanges() returns the number of rows affected
+            _context.SaveChanges();
+            return item.ReportingMark;
+        }
+
     }
 }
 #endregion
--- TrainWebApp/Pages/CRUD.cshtml.cs
+++ TrainWebApp/Pages/CRUD.cshtml.cs
@@ -1,12 +1,123 @@
+#nullable disable
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Drawing.Printing;
+using TrainWatchSystem.BLL;
+using TrainWatchSystem.Entities;
 
 namespace TrainWebApp.Pages
 {
     public class CRUDModel : PageModel
     {
+        #region Private service fields & class constructor
+            private readonly ILogger<IndexModel> _logger;
+            private readonly RollingStockServices _rollingStockServices;
+            private readonly TrainWatchServices _trainWatchServices;
+
+            public CRUDModel(ILogger<IndexModel> logger, RollingStockServices rollingStockServices, TrainWatchServices trainWatchServices)
+            {
+                _logger = logger;
+                _rollingStockServices = rollingStockServices;
+                _trainWatchServices = trainWatchServices;
+            }
+            #endregion
+
+            [TempData]
+            public string Feedback { get; set; }
+
+            public bool HasFeedback => !string.IsNullOrWhiteSpace(Feedback);
+
+            public string ErrorMessage { get; set; }
+
+            public bool HasError => !string.IsNullOrWhiteSpace(ErrorMessage);
+
+            [BindProperty(SupportsGet = true)]
+
+            public string ReportingMark { get; set; }
+
+            [BindProperty(SupportsGet = true)]
+
+            public string SearchArg { get; set; }
+
+            [BindProperty(SupportsGet = true)]
+            public int? searcharg{ get; set; }
+
+            [BindProperty]
+
+            public RollingStock RollingStockInfo { get; set; }
+
+            [BindProperty]
+            public List<RailCarType> RailCarTypes { get; set; }
+
+            [BindProperty(SupportsGet = true)]
+            public int RailCarTypeID { get; set; }
+
+        private void PopulateLists()
+        {
+            RailCarTypes = _rollingStockServices.RailCarTypeList();
+        }
+
         public void OnGet()
         {
+            //  The OnGet executes the first time the page is generated
+            //      then on each Get request issued  by the page (such as on RedirectToPage())
+            PopulateLists();
+
+            if (!string.IsNullOrWhiteSpace(ReportingMark))
+            {
+                RollingStockInfo = _rollingStockServices.GetByID(ReportingMark);
+            }
+
         }
+
+        public IActionResult OnPostDelete()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string ReportingMark = _rollingStockServices.RollingStock_DeleteStock(RollingStockInfo);
+                    if (ReportingMark != null)
+                    {
+                        Feedback = $"Reporting Mark ({ReportingMark}) is no longer in use.";
+                    }
+                    else
+                    {
+                        Feedback = "No Rolling Stock was affected.  Refresh search and try again.";
+                    }
+                    return RedirectToPage(new { ReportingMark = ReportingMark });
+                }
+                catch (ArgumentNullException ex)
+                {
+                    ErrorMessage = GetInnerException(ex).Message;
+                    PopulateLists();
+                    return Page();
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage = GetInnerException(ex).Message;
+                    PopulateLists();
+                    return Page();
+                }
+            }
+
+            return Page();
+        }
+
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+
+            }
+            return ex;
+        }
+
+
+
     }
+    
 }
